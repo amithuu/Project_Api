cd8f682f676ff5a2188f46d4ade9b960bd81744a
amithuu <amithkulkarni99@gmail.com>
2023-08-24 18:54:14 +0530

    feta:new change

--- movies/imdb/models.py
+++ movies/imdb/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Movie_Post(models.Model):
     movie_name = models.CharField(max_length=20)
     movie_desc = models.TextField()
